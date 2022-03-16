1ba9b9e23824693a4b42af290e028ca7f4aef200
chougules739@gmail.com <chougules739@gmail.com>
2022-03-16 12:39:19 +0000

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,16 @@
+namespace API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message = null, string details = null)
+        {
+            Details = details;
+            Message = message;
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
